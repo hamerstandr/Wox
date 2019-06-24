69268ab419d8f8417b7fc74e2419cb91710d55e3
hamed <hamerstandr@yahoo.com>
2019-06-24 19:43:58 +0430

    Color Picer

---
+++ Plugins/ColorPicker/Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wox.Plugin;
+
+namespace Colorpicker
+{
+    class Main : IPlugin
+    {
+        readonly string Location = typeof(Main).Assembly.Location;
+        string Directory ="";
+        public List<Result> Query(Query query)
+        {
+            
+            var result = new Result
+            {
+                Title = "Color Picker",
+                SubTitle = $"Query: {query.Search}",
+                IcoPath = Directory+"\\Images\\app.png",
+                Action = _ =>
+                {
+                    Process.Start(Directory + "\\DesktopColorPicker.exe");
+                    return true;
+                }
+            };
+            return new List<Result> { result };
+        }
+
+        public void Init(PluginInitContext context)
+        {
+            Directory = Path.GetDirectoryName(Location);
+        }
+    }
+}
